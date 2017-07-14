3bb284b716165e5aaa59b3ec76ac03e356bf85e9
Yuyijing <wendyzacknick@gmail.com>
2017-07-14 15:48:19 +0800

    Add files via upload

---
+++ message.js
@@ -0,0 +1,19 @@
+//刪除留言
+function msg_delete(msg_ID) {
+	// var del_fg = false;
+	var del_fg = confirm("確定要刪除嗎?");
+	if(del_fg) {
+		$.ajax({
+			url: "/delete",
+			data: {id:msg_ID},
+			type: "get",
+			dataType: 'text',
+			success: function (msg) {
+				if (msg == "OK") {
+					location.reload();
+					// alert("已刪除");
+				}
+			}
+		});
+	}
+}
